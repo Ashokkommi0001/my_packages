28e99345c5abdbb9fabb80b1977e6631af713db7
Ashokkommi0001 <77483648+Ashokkommi0001@users.noreply.github.com>
2021-03-03 17:59:24 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from  setuptools  import setup
+setup(name='my_packages',
+version='0.1',
+description='creat a package',
+url=https://github.com/Ashokkommi0001/Python-string-methods.git,
+author='ashokkommi',
+author_email='ashokkommigmail.com',
+license='MIT',
+packages=['my_packages'],
+zip_safe=False)
